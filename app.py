b2f28e6d1c4f4100a486aa1bcb4c4e4f382f585a
kanade <xwzkanade@gmail.com>
2020-10-18 07:04:03 -0700

    AddFile

--- app.py
+++ app.py
@@ -8,29 +8,45 @@ app = Flask(__name__)
 
 cred = credentials.Certificate('key.json')
 # Initialize the app with a service account, granting admin privileges
-firebase_admin.initialize_app(cred, {
+
+if not firebase_admin._apps:
+    cred = credentials.Certificate('key.json')
+    default_app = firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://diaryexchanger.firebaseio.com/'
 })
 
 ref = db.reference('')
 
-@app.route('/temp')
+@app.route('/SingleNotebook')
 def hello_world():
-    return 'Hello World!'
+    return render_template('SingleNotebook.html')
 
-@app.route('/', methods = ['POST','GET'])
-def mainPage():
-    return render_template('Test.html')
+@app.route('/')
+def mainpage():
+    return render_template('mainPage.html')
 
-@app.route('/adder', methods = ['POST'])
-def adder():
-    input = request.form.get('nm')
-    print(input)
-    ref.push({
-        'author': input,
-        'title': 'Announcing COBOL, a New Programming Language'
-    })
-    return '111'
+@app.route('/addNew')
+def addNew():
+    return render_template('addNew.html')
+
+@app.route('/Community')
+def Community():
+    return render_template('Community.html')
+
+@app.route('/calender')
+def calender():
+    return render_template('calender.html')
+
+
+#@app.route('/adder', methods = ['POST'])
+#def adder():
+#   input = request.form.get('nm')
+#   print(input)
+#   ref.push({
+#       'author': input,
+#       'title': 'Announcing COBOL, a New Programming Language'
+#   })
+#   return '111'
 
 if __name__ == '__main__':
     app.run()
---
+++ static/mainpage.js
@@ -0,0 +1,4 @@
+function clicck() {
+    alert("Handler for .click() called.");
+    window.location.href = "SingleNotebook.html";
+}
